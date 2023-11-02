9fb670a9b7747773c3f056476877feceef39765b
roibin-fox <robin-fox@sohu.com>
2023-11-02 20:17:35 +0800

    框架
    一个带SDK的程序框架

---
+++ chatsdk/src/main/java/com/bird2fish/chatsdk/view/MessageActivity.java
@@ -0,0 +1,16 @@
+package com.bird2fish.chatsdk.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bird2fish.chatsdk.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
